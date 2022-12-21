caacd5a3dd4edaf1b38581e38b5ff5b6e0084dd5
AndreaRottman <117392926+AndreaRottman@users.noreply.github.com>
2022-12-20 18:34:33 -0600

    Date code added
    date code is added,

--- Ch3Sunshine.java
+++ Ch3Sunshine.java
@@ -1,4 +1,6 @@
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 /*
@@ -6,7 +8,7 @@ Andrea Rottman
 Susan Wilson
 Java
 Decemeber 14, 2022
-
+....IDK
 Gets input minutes for a rental and displays, hours, additional minutes, and rental cost
  */
 public class Ch3Sunshine {
@@ -19,17 +21,35 @@ public class Ch3Sunshine {
 
     public static void main(String[] args) {
         init();
+        LocalDate today=LocalDate.now();
+        String strDate;
+        strDate= getDate();
+        formatDate(strDate);
         int iMinutes = input();
         calcs(iMinutes);
-        output();
+        output(today);
+    }
+
+    private static void formatDate(String strDate) {
+        String enterPattern="mm/dd/yyyy";
+        String newPattern="MMMM dd, yyyy";
+        SimpleDateFormat sdf= new SimpleDateFormat(enterPattern);
+    }
+
+    private static String getDate() {
+        String enterDate;
+        System.out.println("Enter a date(MM/DD/YYYY)");
+        enterDate=inputDevice.nextLine();
+        return enterDate;
     }
 
     public static void init() {
          inputDevice = new Scanner(System.in);
          dollarAmt = new DecimalFormat("$#,###.00");
+
     }
 
-    private static int input() {
+    public static int input() {
         int iMinutes;
         System.out.println("Enter minutes you are renting the equiptment for: ");
         String iData = inputDevice.nextLine();
@@ -51,8 +71,10 @@ public class Ch3Sunshine {
         rentalCost=(40.00*hours)+(1.00*addMinutes);
     }
 
-    private static void output() {
+    public static void output(String today) {
         oRentalCost=dollarAmt.format(rentalCost);
+        System.out.println("Date: "+ today);
+        System.out.println("Sale date: " + enterDate);
         CompanyMotto.getMotto();
         System.out.printf("%-5s%-5s%-18s%-5s%-11s%-5s\n", "Hours","", "Additional Minutes","","Rental Cost", "");
         System.out.printf("%1s%2s%16s%2s%6s%14s%5s", "",hours,"",addMinutes,"",oRentalCost,"");
